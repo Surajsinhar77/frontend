2e38a60e2c5b9ee5a323fba5799eb6fcab3146b5
Suraj Kumar <surajsinhaking77@gmail.com>
2024-04-29 22:48:12 +0530

    done

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+import Home from './Home.jsx';
+
+
+function App() {
+
+  return (
+    <div className="app"> 
+      <Home/>
+      <div>  App page 
+        <h1>heading one </h1>
+        <h1>heading tow </h1>
+        {a}
+        <div className='app1'> 
+
+        </div>
+        <div> 
+          third div
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
