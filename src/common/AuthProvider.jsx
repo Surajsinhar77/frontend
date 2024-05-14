f4b87eaca3a430344f4b47a5181466b3857e45fc
Suraj Kumar <surajsinhaking77@gmail.com>
2024-05-14 21:24:22 +0530

    no major change

--- src/common/AuthProvider.jsx
+++ src/common/AuthProvider.jsx
@@ -1,15 +1,13 @@
 import { useContext, createContext, useState } from "react";
-
 const userContext = createContext();
-
 
 export default function AuthProvider({children}){
     const [data, setData] = useState([]);
     const [search , setSearch] = useState('');
     const [searchData, setSearchData ] = useState([]);
 
+
     function searchItem(){
-        console.log("Serching is trigger")
         const Data = data.filter((item, index)=> 
             item?.name?.toLowerCase().includes(search.toLowerCase())
         )
@@ -35,4 +33,4 @@ export default function AuthProvider({children}){
 }
 
 
-export const useAuth = () => useContext(userContext);
+export function useAuth() {return useContext(userContext)};
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom';
 import AuthProvider from './common/AuthProvider.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
 
   <AuthProvider>
